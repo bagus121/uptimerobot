303f479733de72b749a64f964c2073d71e5a4f4e
bagus121 <67302709+bagus121@users.noreply.github.com>
2020-09-01 14:50:41 +0700

    Update index.js

--- index.js
+++ index.js
@@ -1,7 +1,14 @@
 const axios = require("axios")
 const urls = ["https://glow-bald-order.glitch.me"]
+const urls1 = [https://mixland.glitch.me]
 setInterval(function() {
             urls.forEach(url => {
+            axios.get(url).then(console.log("Pong at " + Date.now())).catch(() => {});
+        })
+    }, 60 * 1000);
+               
+               setInterval(function() {
+            urls1.forEach(url => {
             axios.get(url).then(console.log("Pong at " + Date.now())).catch(() => {});
         })
     }, 60 * 1000);
